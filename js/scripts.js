4109a86a4e468504f16e5d8a0124e0e3883a8c95
Salmoonsamuel <56946917+Salmoonsamuel@users.noreply.github.com>
2021-04-17 22:44:02 -0400

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+/* eslint-disable strict */
+// eslint-disable-next-line strict
+// alert('hello');
+
+
+$(function(){
+  $('.carousel').carousel( {interval: 2000} );
+  $('#carouselButton').on('click', function(){
+    if($('#carouselButton').children('i').hasClass('fa-pause')){
+      $('.carousel').carousel('pause');
+      $('#carouselButton').children('i').removeClass('fa-pause');
+      $('#carouselButton').children('i').addClass('fa-play');
+    } else {
+      $('.carousel').carousel('cycle');
+      $('#carouselButton').children('i').removeClass('fa-play');
+      $('#carouselButton').children('i').addClass('fa-pause');
+    }
+  });
+  $('#reserveButton').on('click', function(){
+    $('#reserveModal').modal('show');
+  });
+  $('#loginButton').on('click', function(){
+    $('#loginModal').modal('show');
+  })
+
+});
+
+// $(function()){
+//   $('#reserveButton').on('click', function(){
+
+//   })
+// }
